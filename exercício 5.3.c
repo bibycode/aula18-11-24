9d614750b0dd4a499d260579ab773400328e7cb8
Ana Beatriz <anabeatrizsantossousa09@gmail.com>
2024-11-25 09:12:59 -0300

    Add files via upload

---
+++ exercício 5.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+float c, j;
+int p, i;
+
+printf("Quantidade de capital?  ");
+scanf("%f", &c);
+
+printf("Juros? ");
+scanf("%f", &j);
+
+printf("Periodo? ");
+scanf("%d", &p);
+for(i=1; i<=p; i++) {
+  c= (1+j/100)*c;
+  printf("%do.mes: R$ %.2f\n", i, c);
+}
+}
